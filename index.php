915ce77cb4f5ed229f10f50e6e315343ead3713d
Teddy <t.boirin@icloud.com>
2019-08-15 16:36:59 +0200

    finish page home

--- index.php
+++ index.php
@@ -1,42 +1,289 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Baï-Bao - Agence de communication à Paris</title>
-  <link rel="stylesheet" href="./css/basic/style.css">
-  <script defer src="./js/app.js"></script>
-</head>
-
-<body>
-  <header>
-<?php include('./inc/header.php')?>
-    <div class="header_secondary">
-      <img src="./assets/logo_framboise.svg" alt="logo">
-      <p>Baï-Bao - Agence de communication - Paris</p>
-      <p>création - web - conseil - production</p>
-      <div class="round_framb"></div>
-      <div class="round_green"></div>
-    </div>
-  </header>
-  <section>
-      <div class="menu_second">
-          <div class="pronoms">
-            <img src="./assets/arrow_left.svg" alt="arrow">
-            <span>Je</span>
-            <span class="hidden">Je</span>
-            <span class="hidden">Je</span>
-            <span class="hidden">Je</span>
-            <span class="hidden">Je</span>
-            <span class="hidden">Je</span>
-            <img src="./assets/arrow_right.svg" alt="arrow">
-          </div>
-          <a href=""><div class="button">Accéder</div></a>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>Baï-Bao - Agence de communication à Paris</title>
+		<link rel="stylesheet" href="./css/basic/style.css">
+		<script defer src="./js/app.js"></script>
+	</head>
+	<body>
+		<header>
+			<?php include('./inc/header.php')?>
+			<div class="header_secondary">
+				<img src="./assets/logo_framboise.svg" alt="logo">
+				<p>
+					Baï-Bao - Agence de communication - Paris
+				</p>
+				<p>
+					création - web - conseil - production
+				</p>
+				<div class="round_framb"></div>
+				<div class="round_green"></div>
+			</div>
+		</header>
+		<section>
+			<div class="menu_second">
+				<div class="pronoms">
+					<img src="./assets/arrow_left.svg" alt="arrow">
+					<span>Je</span>
+					<span class="hidden">Je</span>
+					<span class="hidden">Je</span>
+					<span class="hidden">Je</span>
+					<span class="hidden">Je</span>
+					<span class="hidden">Je</span>
+					<img src="./assets/arrow_right.svg" alt="arrow">
+				</div>
+				<a href=""><div class="button">Accéder</div></a>
+			</div>
+			<div class="nos_offres">
+				<h2>Nos offres</h2>
+				<div class="round"></div>
+				<p class="offre_desc">
+					Baï-Bao est une agence de communication digitale et print indépendante basée à Paris. De la conception au développement de sites internet en passant par un suivi social media et e-réputation, nous vous accompagnons dans la création et le déploiement de votre stratégie de branding sur l'ensemble de vos territoires de communication.
+				</p>
+				<div class="list_offres">
+					<div class="offres_items">
+						<img src="./assets/pictos/site.svg" alt="site">
+						<p class="items__title">
+							SITE INTERNET
+						</p>
+						<p class="items__desc">
+							Photo, vidéo & texte Illustration & animation
+						</p>
+					</div>
+					<div class="offres_items">
+						<img src="./assets/pictos/print.svg" alt="print">
+						<p class="items__title">
+							PRINT
+						</p>
+						<p class="items__desc">
+							Édition Événementiel
+						</p>
+					</div>
+					<div class="offres_items">
+						<img src="./assets/pictos/referencement.svg" alt="referencement">
+						<p class="items__title">
+							RÉFÉRENCEMENT
+						</p>
+						<p class="items__desc">
+							Naturel (SEO) Payant (SEA)
+						</p>
+					</div>
+					<div class="offres_items">
+						<img src="./assets/pictos/branding.svg" alt="branding">
+						<p class="items__title">
+							BRANDING
+						</p>
+						<p class="items__desc">
+							Identité visuelle Création de marque
+						</p>
+					</div>
+					<div class="offres_items">
+						<img src="./assets/pictos/production.svg" alt="production">
+						<p class="items__title">
+							PRODUCTION
+						</p>
+						<p class="items__desc">
+							Photo, vidéo & texte Illustration & animation
+						</p>
+					</div>
+					<div class="offres_items">
+						<img src="./assets/pictos/socialMedia.svg" alt="socialMedia">
+						<p class="items__title">
+							SOCIAL MEDIA
+						</p>
+						<p class="items__desc">
+							Community managment Blogs et forums
+						</p>
+					</div>
+					<div class="offres_items">
+						<img src="./assets/pictos/webdesign.svg" alt="webdesign">
+						<p class="items__title">
+							WEBDESIGN
+						</p>
+						<p class="items__desc">
+							Full responsive Web marketing
+						</p>
+					</div>
+					<div class="offres_items">
+						<img src="./assets/pictos/ereputation.svg" alt="e-reputation">
+						<p class="items__title">
+							E-REPUTATION
+						</p>
+						<p class="items__desc">
+							Audit & veille Conseil & création
+						</p>
+					</div>
+					<div class="offres_items">
+						<img src="./assets/pictos/creationgraphique.svg" alt="creation_graphique">
+						<p class="items__title">
+							CRÉATION GRAPHIQUE
+						</p>
+						<p class="items__desc">
+							Inspiration Déclinaison
+						</p>
+					</div>
+				</div>
       </div>
-  </section>
-  <?php include('./inc/footer.php')?>
-</body>
+      
+      <div class="stats">
+        <div class="stats_items">
+          <span class="stats__number">9</span>
+          <span class="stats__def">ans d'existence</span>
+        </div>
 
+        <div class="stats_items">
+          <span class="stats__number turquoise">67</span>
+          <span class="stats__def">clients happy</span>
+        </div>
+
+        <div class="stats_items">
+          <span class="stats__number">32</span>
+          <span class="stats__def">ans de moyenne d'âge</span>
+        </div>
+
+        <div class="stats_items">
+          <span class="stats__number">4</span>
+          <span class="stats__def">stats en page d'accueil</span>
+        </div>
+      </div>
+      <!-- end of stats -->
+
+      <div class="portraits">
+        <div class="forme_design">
+          <img src="./assets/form.svg" alt="form">
+        </div>
+        <div class="communication">
+          <p><img src="./assets/logo_black.svg" alt="logo"></p>
+          <p><img src="./assets/linkedin_beige.svg" alt="linkedin"></p>
+        </div>
+        <h2>PORTRAIT D'UN BAÏ-BAO</h2>
+        <p class="paragraph">
+        Le Baï-Bao, acronyme désopilant de « Boîte A Idées, Boîte A Outils », est un être paradoxal, aussi détendu et primesautier de nature que dynamique et sérieux dans l’exercice de ses missions.
+        ...il vous en faut plus ?
+        <a href=""><div class="button">Accéder</div></a>
+        </p>
+      </div>
+
+      <!-- end of portrait -->
+
+      <div class="realisations">
+          <h2>NOS RÉALISATIONS</h2>
+          <div class="round"></div>
+          <div class="realisations__list">
+          <div class="realisations__items">
+                <p class="item_title">1990 Architectures</p>
+                <p class="item_desc">Très pratique si vous avez une église délabrée au fond du jardin.</p>
+              </div>
+
+              <div class="realisations__items realisations__items--turquoise">
+                <p class="item_title item_title--dark">SFIL</p>
+                <p class="item_desc item_desc--dark">Rapport RSE</p>
+              </div>
+
+              <div class="realisations__items">
+                <p class="item_title">ARTHUR LUIZ PIZA</p>
+                <p class="item_desc">catalogue raisonné</p>
+              </div>
+          </div>
+          <a href=""><div class="button">En voir +</div></a>
+      </div>
+
+      <!-- end of realisations -->
+
+      <div class="references">
+        <h2>NOS RÉFÉRENCES</h2>
+        <div class="round"></div>
+        <div class="list_references">
+            <div class="two_items">
+            <img src="./assets/diderot.svg" alt="diderot">
+            <img src="./assets/sorbonne.svg" alt="sorbonne">
+            </div>
+            <div class="two_items">
+            <img src="./assets/sfil.svg" alt="sfil">
+            <img src="./assets/cafil.svg" alt="cafil">
+            </div>
+              <div class="references_txt">
+                <h3>Ils nous font confiance</h3>
+                <p class="paragraph">
+                Des banques, des fonds, des associations, des entreprises, grandes et moins grandes… de la holding à la TPE, il règne une joyeuse diversité dans notre éventail de références. Autant de problématiques de communication différentes qui renouvellent les sensations et qui chauffent nos méninges pour cogiter THE solution of THE solution, adaptée à chaque univers et à chaque objectif.
+                </p>
+              </div>
+        </div>
+      </div>
+      <!-- end of références -->
+      <div class="news">
+      <h1>DERNIÈRES ACTUALITÉS</h1>
+        <div class="news_container">
+        
+        <div class="news_item">
+          <div class="header_new">
+            <img src="./assets/article.png" alt="new">
+          </div>
+          <h3>Speed Update : les sites les plus lents désormais pénalisés en référencement naturel par Google...</h3>
+          <p>Début 2018, Google a présenté une mise à jour de son algorithme Speed Update destinée à pénaliser les sites jugés trop lents sur mobile. Devenue effective sur toute la toile en juillet 2018, il convient de relativiser la portée de cette initiative de Google sur les sites les moins performants en la matière. [...]</p>
+          <div class="share">
+              <img src="./assets/twitter.svg" alt="twitter">
+              <img src="./assets/fb.svg" alt="fb">
+              <span class="date">
+              posté le 23 janvier 2019
+              </span>
+          </div>
+        </div>
+        <!--  -->
+        <div class="news_item">
+          <div class="header_new">
+            <img src="./assets/article.png" alt="new">
+          </div>
+          <h3>Speed Update : les sites les plus lents désormais pénalisés en référencement naturel par Google...</h3>
+          <p>Début 2018, Google a présenté une mise à jour de son algorithme Speed Update destinée à pénaliser les sites jugés trop lents sur mobile. Devenue effective sur toute la toile en juillet 2018, il convient de relativiser la portée de cette initiative de Google sur les sites les moins performants en la matière. [...]</p>
+          <div class="share">
+              <img src="./assets/twitter.svg" alt="twitter">
+              <img src="./assets/fb.svg" alt="fb">
+              <span class="date">
+              posté le 23 janvier 2019
+              </span>
+          </div>
+        </div>
+
+         <!--  -->
+         <div class="news_item">
+          <div class="header_new">
+            <img src="./assets/article.png" alt="new">
+          </div>
+          <h3>Speed Update : les sites les plus lents désormais pénalisés en référencement naturel par Google...</h3>
+          <p>Début 2018, Google a présenté une mise à jour de son algorithme Speed Update destinée à pénaliser les sites jugés trop lents sur mobile. Devenue effective sur toute la toile en juillet 2018, il convient de relativiser la portée de cette initiative de Google sur les sites les moins performants en la matière. [...]</p>
+          <div class="share">
+              <img src="./assets/twitter.svg" alt="twitter">
+              <img src="./assets/fb.svg" alt="fb">
+              <span class="date">
+              posté le 23 janvier 2019
+              </span>
+          </div>
+        </div>
+
+         <!--  -->
+         <div class="news_item">
+          <div class="header_new">
+            <img src="./assets/article.png" alt="new">
+          </div>
+          <h3>Speed Update : les sites les plus lents désormais pénalisés en référencement naturel par Google...</h3>
+          <p>Début 2018, Google a présenté une mise à jour de son algorithme Speed Update destinée à pénaliser les sites jugés trop lents sur mobile. Devenue effective sur toute la toile en juillet 2018, il convient de relativiser la portée de cette initiative de Google sur les sites les moins performants en la matière. [...]</p>
+          <div class="share">
+              <img src="./assets/twitter.svg" alt="twitter">
+              <img src="./assets/fb.svg" alt="fb">
+              <span class="date">
+              posté le 23 janvier 2019
+              </span>
+          </div>
+        </div>
+
+        
+        </div>
+      </div>
+		</section>
+		<?php include('./inc/footer.php')?>
+	</body>
 </html>
