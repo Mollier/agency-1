2c9e5b827c4cc2c84a9253ee2d397e92dc2923a1
Teddy <t.boirin@icloud.com>
2019-08-16 13:02:19 +0200

    finish page realisations

--- realisations.php
+++ realisations.php
@@ -1,189 +1,214 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta http-equiv="X-UA-Compatible" content="ie=edge">
-		<title>Baï-Bao - Notre équipe</title>
-		<link rel="stylesheet" href="./css/basic/style.css">
-		<script defer src="./js/app.js"></script>
-	</head>
-	<body>
-		<header>
-			<?php include('./inc/header.php')?>
-			<div class="header_secondary header_secondary--team">
-				<h1>nos réalisations</h1>
-				<div class="round_middle"></div>
-				<div class="round__turquoise">
 
-				</div>
-		</header>
-		<section>
-      <div class="realisations_top">
-        <p class="paragraph">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Baï-Bao - Notre équipe</title>
+	<link rel="stylesheet" href="./css/basic/style.css">
+	<script defer src="./js/app.js"></script>
+</head>
+
+<body>
+	<header>
+		<?php include('./inc/header.php') ?>
+		<div class="header_secondary header_secondary--team">
+			<h1>nos réalisations</h1>
+			<div class="round_middle"></div>
+			<div class="round__turquoise">
+
+			</div>
+	</header>
+	<section>
+		<div class="realisations_top">
+			<p class="paragraph">
 				Des banques, des fonds, des associations, des entreprises, grandes et moins grandes… de la holding à la TPE, il règne une joyeuse diversité dans notre éventail de références. Autant de problématiques de communication qui renouvellent les sensations et réchauffent nos méninges pour cogiter ZE solution of ZE solution.
-				</p>
-				<div class="realisations_filter">
-					<div class="filter_item">
-						<span>Artistes</span>
+			</p>
+			<div class="realisations_filter">
+				<div class="filter_item">
+					<span>Artistes</span>
+				</div>
+				<div class="filter_item">
+					<span>Institutions</span>
+				</div>
+				<div class="filter_item">
+					<span>Éducation</span>
+				</div>
+				<div class="filter_item">
+					<span>IT</span>
+				</div>
+				<div class="filter_item">
+					<span>Associations</span>
+				</div>
+				<div class="filter_item">
+					<span>Cabinets</span>
+				</div>
+			</div>
+		</div>
+
+		<div class="realisations">
+			<div class="round__beige"></div>
+			<div class="round__beige--right"></div>
+			<div class="realisations__list">
+				<div class="realisations__items">
+					<p class="item_title">1990 Architectures</p>
+					<p class="item_desc">Très pratique si vous avez une église délabrée au fond du jardin.</p>
+				</div>
+
+				<div class="realisations__items realisations__items--turquoise">
+					<p class="item_title item_title--dark">SFIL</p>
+					<p class="item_desc item_desc--dark">Rapport RSE</p>
+				</div>
+
+				<div class="realisations__items realisations__items--beige">
+					<p class="item_title item_title--dark">ARTHUR LUIZ PIZA</p>
+					<p class="item_desc item_desc--dark">catalogue raisonné</p>
+				</div>
+
+				<div class="realisations__items">
+					<p class="item_title">ADYTON</p>
+					<p class="item_desc">Très pratique si vous avez une église délabrée au fond du jardin.</p>
+				</div>
+
+				<div class="realisations__items">
+					<p class="item_title">ALBERT MESLEY</p>
+					<p class="item_desc">calalogue raisonné</p>
+				</div>
+
+				<div class="realisations__items realisations__items--beige">
+					<p class="item_title item_title--dark">LE BRETON</p>
+					<p class="item_desc item_desc--dark">calalogue raisonné</p>
+				</div>
+
+				<div class="realisations__items">
+					<p class="item_title">BAZIN CAZELLES</p>
+					<p class="item_desc">calalogue raisonné</p>
+				</div>
+
+				<div class="realisations__items">
+					<p class="item_title">CAFFIL</p>
+					<p class="item_desc">calalogue raisonné</p>
+				</div>
+
+				<div class="realisations__items realisations__items--turquoise">
+					<p class="item_title item_title--dark">CODEH EXECUTIVE</p>
+					<p class="item_desc item_desc--dark">calalogue raisonné</p>
+				</div>
+			</div>
+		</div>
+
+		<!-- references -->
+
+		<div class="references">
+			<h2>NOS RÉFÉRENCES</h2>
+			<div class="round"></div>
+			<div class="list_references">
+				<div class="container__ref">
+					<div class="two_items">
+						<img src="./assets/diderot.svg" alt="diderot">
+						<img src="./assets/sorbonne.svg" alt="sorbonne">
 					</div>
-					<div class="filter_item">
-						<span>Institutions</span>
+					<div class="two_items">
+						<img src="./assets/sfil.svg" alt="sfil">
+						<img src="./assets/cafil.svg" alt="cafil">
 					</div>
-					<div class="filter_item">
-						<span>Éducation</span>
+					<div class="two_items">
+						<img src="./assets/diderot.svg" alt="diderot">
+						<img src="./assets/sorbonne.svg" alt="sorbonne">
 					</div>
-					<div class="filter_item">
-						<span>IT</span>
+					<div class="two_items">
+						<img src="./assets/sfil.svg" alt="sfil">
+						<img src="./assets/cafil.svg" alt="cafil">
 					</div>
-					<div class="filter_item">
-						<span>Associations</span>
+					<div class="two_items">
+						<img src="./assets/diderot.svg" alt="diderot">
+						<img src="./assets/sorbonne.svg" alt="sorbonne">
 					</div>
-					<div class="filter_item">
-						<span>Cabinets</span>
+					<div class="two_items">
+						<img src="./assets/sfil.svg" alt="sfil">
+						<img src="./assets/cafil.svg" alt="cafil">
+					</div>
+					<div class="two_items">
+						<img src="./assets/diderot.svg" alt="diderot">
+						<img src="./assets/sorbonne.svg" alt="sorbonne">
+					</div>
+					<div class="two_items">
+						<img src="./assets/sfil.svg" alt="sfil">
+						<img src="./assets/cafil.svg" alt="cafil">
+					</div>
+					<div class="two_items">
+						<img src="./assets/diderot.svg" alt="diderot">
+						<img src="./assets/sorbonne.svg" alt="sorbonne">
+					</div>
+					<div class="two_items">
+						<img src="./assets/sfil.svg" alt="sfil">
+						<img src="./assets/cafil.svg" alt="cafil">
+					</div>
+					<div class="two_items">
+						<img src="./assets/diderot.svg" alt="diderot">
+						<img src="./assets/sorbonne.svg" alt="sorbonne">
+					</div>
+					<div class="two_items">
+						<img src="./assets/sfil.svg" alt="sfil">
+						<img src="./assets/cafil.svg" alt="cafil">
+					</div>
+					<div class="two_items">
+						<img src="./assets/diderot.svg" alt="diderot">
+						<img src="./assets/sorbonne.svg" alt="sorbonne">
+					</div>
+					<div class="two_items">
+						<img src="./assets/sfil.svg" alt="sfil">
+						<img src="./assets/cafil.svg" alt="cafil">
+					</div>
+					<div class="two_items">
+						<img src="./assets/diderot.svg" alt="diderot">
+						<img src="./assets/sorbonne.svg" alt="sorbonne">
+					</div>
+					<div class="two_items">
+						<img src="./assets/sfil.svg" alt="sfil">
+						<img src="./assets/cafil.svg" alt="cafil">
 					</div>
 				</div>
-      </div>
-
-			<div class="realisations">
-				<div class="round__beige"></div>
-				<div class="round__beige--right"></div>
-          <div class="realisations__list">
-          <div class="realisations__items">
-                <p class="item_title">1990 Architectures</p>
-                <p class="item_desc">Très pratique si vous avez une église délabrée au fond du jardin.</p>
-              </div>
-
-              <div class="realisations__items realisations__items--turquoise">
-                <p class="item_title item_title--dark">SFIL</p>
-                <p class="item_desc item_desc--dark">Rapport RSE</p>
-              </div>
-
-              <div class="realisations__items realisations__items--beige">
-                <p class="item_title item_title--dark">ARTHUR LUIZ PIZA</p>
-                <p class="item_desc item_desc--dark">catalogue raisonné</p>
-              </div>
-
-							<div class="realisations__items">
-                <p class="item_title">ADYTON</p>
-                <p class="item_desc">Très pratique si vous avez une église délabrée au fond du jardin.</p>
-              </div>
-
-							<div class="realisations__items">
-                <p class="item_title">ALBERT MESLEY</p>
-                <p class="item_desc">calalogue raisonné</p>
-              </div>
-
-							<div class="realisations__items realisations__items--beige">
-                <p class="item_title item_title--dark">LE BRETON</p>
-                <p class="item_desc item_desc--dark">calalogue raisonné</p>
-              </div>
-
-							<div class="realisations__items">
-                <p class="item_title">BAZIN CAZELLES</p>
-                <p class="item_desc">calalogue raisonné</p>
-              </div>
-
-							<div class="realisations__items">
-                <p class="item_title">CAFFIL</p>
-                <p class="item_desc">calalogue raisonné</p>
-              </div>
-
-							<div class="realisations__items realisations__items--turquoise">
-                <p class="item_title item_title--dark">CODEH EXECUTIVE</p>
-                <p class="item_desc item_desc--dark">calalogue raisonné</p>
-              </div>
-          </div>
 			</div>
-			
-			<!-- references -->
+		</div>
 
-			<div class="references">
-        <h2>NOS RÉFÉRENCES</h2>
-        <div class="round"></div>
-        <div class="list_references">
-            <div class="container__ref">
-						<div class="two_items">
-            <img src="./assets/diderot.svg" alt="diderot">
-            <img src="./assets/sorbonne.svg" alt="sorbonne">
-            </div>
-            <div class="two_items">
-            <img src="./assets/sfil.svg" alt="sfil">
-            <img src="./assets/cafil.svg" alt="cafil">
-						</div>
-						<div class="two_items">
-            <img src="./assets/diderot.svg" alt="diderot">
-            <img src="./assets/sorbonne.svg" alt="sorbonne">
-            </div>
-            <div class="two_items">
-            <img src="./assets/sfil.svg" alt="sfil">
-            <img src="./assets/cafil.svg" alt="cafil">
-						</div>
-						<div class="two_items">
-            <img src="./assets/diderot.svg" alt="diderot">
-            <img src="./assets/sorbonne.svg" alt="sorbonne">
-            </div>
-            <div class="two_items">
-            <img src="./assets/sfil.svg" alt="sfil">
-            <img src="./assets/cafil.svg" alt="cafil">
-						</div>
-						<div class="two_items">
-            <img src="./assets/diderot.svg" alt="diderot">
-            <img src="./assets/sorbonne.svg" alt="sorbonne">
-            </div>
-            <div class="two_items">
-            <img src="./assets/sfil.svg" alt="sfil">
-            <img src="./assets/cafil.svg" alt="cafil">
-						</div>
-						<div class="two_items">
-            <img src="./assets/diderot.svg" alt="diderot">
-            <img src="./assets/sorbonne.svg" alt="sorbonne">
-            </div>
-            <div class="two_items">
-            <img src="./assets/sfil.svg" alt="sfil">
-            <img src="./assets/cafil.svg" alt="cafil">
-						</div>
-						<div class="two_items">
-            <img src="./assets/diderot.svg" alt="diderot">
-            <img src="./assets/sorbonne.svg" alt="sorbonne">
-            </div>
-            <div class="two_items">
-            <img src="./assets/sfil.svg" alt="sfil">
-            <img src="./assets/cafil.svg" alt="cafil">
-            </div>  <div class="two_items">
-            <img src="./assets/diderot.svg" alt="diderot">
-            <img src="./assets/sorbonne.svg" alt="sorbonne">
-            </div>
-            <div class="two_items">
-            <img src="./assets/sfil.svg" alt="sfil">
-            <img src="./assets/cafil.svg" alt="cafil">
-            </div>  <div class="two_items">
-            <img src="./assets/diderot.svg" alt="diderot">
-            <img src="./assets/sorbonne.svg" alt="sorbonne">
-            </div>
-            <div class="two_items">
-            <img src="./assets/sfil.svg" alt="sfil">
-            <img src="./assets/cafil.svg" alt="cafil">
-            </div>
-						</div>
-        </div>
-			</div>
-			
-			<div class="partenaires">
+		<div class="partenaires">
 			<h2>NOS PARTENAIRES</h2>
-				<div class="round"></div>
-				<div class="partenaires_list">
-					<div class="partenaires_item">
-						<p class="item_title item_title--dark">CAFFIL</p>
-						<p class="item_desc item_desc--dark">calalogue raisonné</p>
-					</div>
-
-					
-
-
+			<div class="round"></div>
+			<div class="partenaires_list">
+				<div class="partenaires_item">
+					<p class="item_title item_title--dark">CAFFIL</p>
+					<p class="item_desc item_desc--dark">calalogue raisonné</p>
 				</div>
 			</div>
-		</section>
-		<?php include('./inc/footer.php')?>
-	</body>
+		</div>
+		<!-- end partenaires -->
+
+		<div class="coups_de_coeur">
+			<h1>NOS COUPS DE COEUR</h1>
+			<div class="coeur"> <img src="./assets/coeur.svg" alt="coeur"></div>
+			<p class="paragraph paragraph--white">petit paragraphe sur les clients qui nous suivent depuis des années.</p>
+			<div class="coups_de_coeur_list">
+				<div class="coups_de_coeur_item">
+					<p class="item_title item_title--dark">CAFFIL</p>
+					<p class="item_desc item_desc--dark">calalogue raisonné</p>
+				</div>
+
+				<div class="coups_de_coeur_item">
+					<p class="item_title item_title--dark">CAFFIL</p>
+					<p class="item_desc item_desc--dark">calalogue raisonné</p>
+				</div>
+
+				<div class="coups_de_coeur_item">
+					<p class="item_title item_title--dark">CAFFIL</p>
+					<p class="item_desc item_desc--dark">calalogue raisonné</p>
+				</div>
+			</div>
+			<div class="round__beige__coups"></div>
+		</div>
+	</section>
+	<?php include('./inc/footer.php') ?>
+</body>
+
 </html>
