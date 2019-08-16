c15eda894393a40fc792371eded692560524b43f
Teddy <t.boirin@icloud.com>
2019-08-16 15:57:06 +0200

    finish page news

--- inc/menu.php
+++ inc/menu.php
@@ -1,23 +1,21 @@
 <nav class="menu-close" id="menu-close">
-        <ul class="items-menu">
-          <li class="is_in"><a href="index.php">L'agence</a></li>
-            <li><a href="team.php">L'équipe</a></li>
-            <li><a href="realisations.php">Nos réalisations</a></li>
-            <li><a href="index.html">Nos services</a></li>
-            <li><a href="index.html">Blog</a></li>
-            <li><a href="index.html">Contact</a></li>
-            <li><a href="index.html">MON ESPACE</a></li>
-            <li class="no-left">
-              <div class="social">
-                <a href=""><img src="./assets/insta.svg" alt="instagram"></a>
-                <a href=""><img src="./assets/linkedin.svg" alt="linkedin"></a>
-                <div class="separation"></div>
-                <a href="">FR</a>
-                <div class="separation_point"></div>
-                <a href="">EN</a>
-              </div>
-            </li>
-        </ul>
-      </nav>
-
-   
+  <ul class="items-menu">
+    <li class="is_in"><a href="index.php">L'agence</a></li>
+    <li><a href="team.php">L'équipe</a></li>
+    <li><a href="realisations.php">Nos réalisations</a></li>
+    <li><a href="services.php">Nos services</a></li>
+    <li><a href="news.php">Blog</a></li>
+    <li><a href="contact.php">Contact</a></li>
+    <li><a href="profil.php">MON ESPACE</a></li>
+    <li class="no-left">
+      <div class="social">
+        <a href=""><img src="./assets/insta.svg" alt="instagram"></a>
+        <a href=""><img src="./assets/linkedin.svg" alt="linkedin"></a>
+        <div class="separation"></div>
+        <a href="">FR</a>
+        <div class="separation_point"></div>
+        <a href="">EN</a>
+      </div>
+    </li>
+  </ul>
+</nav>
---
+++ services.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Baï-Bao - Notre équipe</title>
+    <link rel="stylesheet" href="./css/basic/style.css">
+    <script defer src="./js/app.js"></script>
+</head>
+
+<body>
+    <header>
+        <?php include('./inc/header.php') ?>
+        <div class="header_secondary ">
+            <div class="logo_service">
+                <img src="./assets/services/site.svg" alt="site">
+                <h3>site internet</h3>
+                <p class="paragraph">
+                    Site Internet • Print • Référencement • Production • Création graphique • Social Media • E-Réputation • Web Design • Branding
+                </p>
+                <div class="round_middle"></div>
+            </div>
+        </div>
+    </header>
+
+    <section>
+
+        <div class="services_content">
+
+        </div>
+    </section>
+    <?php include('./inc/footer.php') ?>
+</body>
+
+</html>
