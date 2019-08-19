4c238215369416e97df679be2cc5cd852e6bfbcc
Teddy <t.boirin@icloud.com>
2019-08-19 16:26:12 +0200

    add /profile

---
+++ profile/connect.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="./css/styles.css">
+  <title>Baï-Bao - Accès à votre espace perso</title>
+</head>
+
+<body>
+  <header>
+    <div class="header__logo">
+      <svg id="Calque_1" data-name="Calque 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 61.08 89.53">
+        <defs>
+        </defs>
+        <title>BB-logo-B</title>
+        <g id="Calque_1-2" data-name="Calque 1-2">
+          <path class="cls-1" d="M0,7.12a7.12,7.12,0,1,1,7.12,7.12A7.12,7.12,0,0,1,0,7.12ZM27.38,31.8V89C15.67,87,6.88,78.83,6.67,65.82h0V21H27.38Zm.2-17.56A7.12,7.12,0,1,1,34.7,7.12a7.12,7.12,0,0,1-7.12,7.12Zm6.87,75.29H33.33V71.81a5.25,5.25,0,0,0,.79.06,6.67,6.67,0,0,0,0-13.31,5.25,5.25,0,0,0-.79.06V41.18h1.12c14.71,0,26.63,8.81,26.63,24.19S49.16,89.53,34.45,89.53Z" transform="translate(0 0)" />
+        </g>
+      </svg>
+    </div>
+    <a href="../index.php">retour au site</a>
+  </header>
+
+  <section class="connect">
+    <form action="#" method="post">
+      <p> <input type="text" name="identifiant" placeholder="identifiant"></p>
+      <p><input type="password" name="password" placeholder="mot de passe"></p>
+      <p><input type="submit" name="send" value="accéder"></p>
+    </form>
+
+  </section>
+</body>
+
+</html>
---
+++ profile/connected.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="./css/styles.css">
+  <title>Baï-Bao - Accès à votre espace perso</title>
+</head>
+
+<body>
+  <header class="header__connected">
+    <div class="header__logo">
+      <svg id="Calque_1" data-name="Calque 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 61.08 89.53">
+        <defs>
+        </defs>
+        <title>BB-logo-B</title>
+        <g id="Calque_1-2" data-name="Calque 1-2">
+          <path class="cls-1" d="M0,7.12a7.12,7.12,0,1,1,7.12,7.12A7.12,7.12,0,0,1,0,7.12ZM27.38,31.8V89C15.67,87,6.88,78.83,6.67,65.82h0V21H27.38Zm.2-17.56A7.12,7.12,0,1,1,34.7,7.12a7.12,7.12,0,0,1-7.12,7.12Zm6.87,75.29H33.33V71.81a5.25,5.25,0,0,0,.79.06,6.67,6.67,0,0,0,0-13.31,5.25,5.25,0,0,0-.79.06V41.18h1.12c14.71,0,26.63,8.81,26.63,24.19S49.16,89.53,34.45,89.53Z" transform="translate(0 0)" />
+        </g>
+      </svg>
+    </div>
+    <p class="bold">Bienvenue</p>
+    <div class="line"></div>
+    <p class="username">tedi</p>
+
+  </header>
+
+  <section class="connected">
+    <div class="options">
+      <div class="connected__item">
+        <a href=""><img src="../assets/profile/devis.svg" alt="devis"></a>
+        <h4>Mes devis</h4>
+        <p>devis de votre site</p>
+      </div>
+      <div class="connected__item">
+        <a href=""><img src="../assets/profile/charte.svg" alt="charte"></a>
+        <h4>CHARTE GRAPHIQUE</h4>
+        <p>charte de votre vite</p>
+      </div>
+      <div class="connected__item">
+        <a href=""><img src="../assets/profile/tuto.svg" alt="tutoriel"></a>
+        <h4>MES TUTORIELS</h4>
+        <p>tutoriel de votre site</p>
+      </div>
+      <div class="connected__item">
+        <a href=""><img src="../assets/profile/factures.svg" alt="factures"></a>
+        <h4>MES FACTURES</h4>
+        <p>factures de votre site</p>
+      </div>
+    </div>
+
+    <a href="">
+      <div class="button">accéder à mes identifiants</div>
+    </a>
+
+    <a href="">
+      <div class="button">accéder à mon Google Analytics</div>
+    </a>
+
+    <span class="need_help--paragraph">Les Baï-Bao qui
+      s’occupent de vous</span>
+
+    <div class="round"></div>
+    <div class="need_help">
+      <div class="need_help_item">
+        <img src="../assets/profile/yves.png" alt="yves">
+        <span class="bold">Yves</span>
+        <p> <span>yves.milon@bai-bao.fr</span></p>
+      </div>
+      <div class="need_help_item">
+        <img src="../assets/profile/zeo.png" alt="zoe">
+        <p> <span class="bold">Zoé</span></p>
+        <p> <span>zoe.franch@bai-bao.fr</span></p>
+      </div>
+    </div>
+  </section>
+</body>
+
+</html>
