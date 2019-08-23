7926eeea10f3ad7a969651d2b8b78670beda1431
Teddy <t.boirin@icloud.com>
2019-08-23 13:04:45 +0200

    add upload charte

--- profile/assets/config/classes/Clients.php
+++ profile/assets/config/classes/Clients.php
@@ -45,12 +45,9 @@ class Clients {
         $charte_file = $_FILES['charte_file']['name'];
 
 
-        if(empty($_FILES['charte_file'])) {
-            if (empty($name) and empty($phone) and empty($email)) {
+        if($_FILES['charte_file']['error'] == 4) {
 
-                echo("Veuillez remplir tout les champs");
-            } else {
-
+            if(!empty($name) AND !empty($phone) AND !empty($email)) {
                 $req = $con->prepare('
                 UPDATE clients
                 SET name = :name, phone = :phone, email = :email WHERE id_client =' . $id);
@@ -59,29 +56,37 @@ class Clients {
                 $req->bindParam(':email', $email);
                 $req->execute();
                 echo"Client modifié";
-            }
-        }
-        else {
-            if (empty($name) and empty($phone) and empty($email)) {
-
-                echo("Veuillez remplir tout les champs");
             } else {
+                echo 'Veuillez remplir tout les champs';
+            }
+
+        } elseif(!empty($_FILES['charte_file'])) { // si le fichier charte_file n'est pas vide
+            echo '2';
+
+            if(!empty($name) AND !empty($phone) AND !empty($email)) {
+
+                // ajout du fichier
+                $this->uploadCharte();
                 $req = $con->prepare('
+                UPDATE chartes
+                SET link = :charte_file WHERE id_client =' . $id);
+                $req->bindParam(':charte_file', $charte_file);
+                $req->execute();
+
+                // ajout des infos
+                $reqTwo = $con->prepare('
                 UPDATE clients
                 SET name = :name, phone = :phone, email = :email WHERE id_client =' . $id);
-                $req->bindParam(':name', $name);
-                $req->bindParam(':phone', $phone);
-                $req->bindParam(':email', $email);
-                $req->execute();
+                $reqTwo->bindParam(':name', $name);
+                $reqTwo->bindParam(':phone', $phone);
+                $reqTwo->bindParam(':email', $email);
+                $reqTwo->execute();
+                echo"Client modifié";
+            } else {
+                echo 'Veuillez remplir tout les champs';
             }
-            $this->uploadCharte();
-            $req = $con->prepare('
-            UPDATE chartes
-            SET link = :charte_file WHERE id_client =' . $id);
-            $req->bindParam(':charte_file', $charte_file);
-            $req->execute();
-            echo"Données modifiées";
         }
+
     }
 
 
@@ -129,56 +134,46 @@ class Clients {
                    echo "Error: " . $_FILES["charte_file"]["error"];
                }
            }
-
     }
 
-    // tutoriels
-
-    public function getTutoriel(PDO $con, $id)
-    {
-        $req = $con->query('SELECT * FROM tutoriels WHERE id_client ='.$id);
-        return $req->fetch(PDO::FETCH_ASSOC);
-    }
+    //tutoriel
 
     public function uploadTutoriel() {
 
-            if($_SERVER["REQUEST_METHOD"] == "POST"){
-                // Vérifie si le fichier a été uploadé sans erreur.
-                if(isset($_FILES["tutoriel"]) && $_FILES["tutoriel"]["error"] == 0){
-                    $allowed = array("pdf" => "application/pdf");
-                    $filename = $_FILES["tutoriel"]["name"];
-                    $filetype = $_FILES["tutoriel"]["type"];
-                    $filesize = $_FILES["tutoriel"]["size"];
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            // Vérifie si le fichier a été uploadé sans erreur.
+            if(isset($_FILES["charte_file"]) && $_FILES["charte_file"]["error"] == 0){
+                $allowed = array("pdf" => "application/pdf");
+                $filename = $_FILES["charte_file"]["name"];
+                $filetype = $_FILES["charte_file"]["type"];
+                $filesize = $_FILES["charte_file"]["size"];
 
-                    // Vérifie l'extension du fichier
-                    $ext = pathinfo($filename, PATHINFO_EXTENSION);
-                    if(!array_key_exists($ext, $allowed)) die("Erreur : Veuillez sélectionner un format de fichier valide.");
+                // Vérifie l'extension du fichier
+                $ext = pathinfo($filename, PATHINFO_EXTENSION);
+                if(!array_key_exists($ext, $allowed)) die("Erreur : Veuillez sélectionner un format de fichier valide.");
 
-                    // Vérifie la taille du fichier - 5Mo maximum
-                    $maxsize = 5 * 1024 * 1024;
-                    if($filesize > $maxsize) die("Error: La taille du fichier est supérieure à la limite autorisée.");
+                // Vérifie la taille du fichier - 5Mo maximum
+                $maxsize = 5 * 1024 * 1024;
+                if($filesize > $maxsize) die("Error: La taille du fichier est supérieure à la limite autorisée.");
 
-                    // Vérifie le type MIME du fichier
-                    if(in_array($filetype, $allowed)){
-                        // Vérifie si le fichier existe avant de le télécharger.
-                        if(file_exists("../assets/upload/tutoriels/" . $_FILES["tutoriel"]["name"])){
-                            echo($_FILES["tutoriel"]["name"] . " existe déjà.");
-                            die();
-                        } else{
-                            move_uploaded_file($_FILES["tutoriel"]["tmp_name"], "../assets/upload/tutoriels/" . $_FILES["tutoriel"]["name"]);
-                            // echo('Votre fichier a été téléchargé avec succès.');
-                        }
+                // Vérifie le type MIME du fichier
+                if(in_array($filetype, $allowed)){
+                    // Vérifie si le fichier existe avant de le télécharger.
+                    if(file_exists("../assets/upload/chartes/" . $_FILES["charte_file"]["name"])){
+                        echo($_FILES["charte_file"]["name"] . " existe déjà.");
+                        die();
                     } else{
-                        echo("Error: Il y a eu un problème de téléchargement de votre fichier. Veuillez réessayer.");
-
+                        move_uploaded_file($_FILES["charte_file"]["tmp_name"], "../assets/upload/chartes/" . $_FILES["charte_file"]["name"]);
+                        // echo('Votre fichier a été téléchargé avec succès.');
                     }
                 } else{
-                    echo "Error: " . $_FILES["tutoriel"]["error"];
+                    echo("Error: Il y a eu un problème de téléchargement de votre fichier. Veuillez réessayer.");
+
                 }
+            } else{
+                echo "Error: " . $_FILES["charte_file"]["error"];
             }
-
-
+        }
     }
-
 
 }
